02a14f6474a0f6a3167c3b2d9b79e2f0c4fa1ab8
Mari Jaoshvili <marijaoshvili@Maris-MacBook-Air.local>
2022-11-16 13:42:03 -0500

    finished all the functions

--- discussion11.py
+++ discussion11.py
@@ -6,6 +6,7 @@ import os
 
 # Create Database
 def setUpDatabase(db_name):
+    #this is how to set up our sql stuff
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
@@ -39,12 +40,15 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+    cur.execute('drop table if exists Patients')
+    cur.execute('create table Patients (pet_id INTEGER PRIMARY KEY, name TEST, species_id NUMBER, age INTEGER, cuteness INTEGER, aggresiveness NUMBER)')
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+
+    cur.execute('insert into Patients (pet_id, name, species_id, age, cuteness, aggresiveness) values(?,?,?,?,?,?)', (0, "Fluffle", 0, 3, 90, 100))  
+    conn.commit()
     
 
 # TASK 2
@@ -58,14 +62,24 @@ def add_pets_from_json(filename, cur, conn):
     f.close()
     json_data = json.loads(file_data)
 
+    species_id_dict = {'Rabbit' : 0, 'Dog': 1, 'Cat': 2, 'Boa Constrictor': 3, 'Chinchilla': 4, 'Hamster': 5, 'Cobra': 6, 'Parrot': 7, 'Shark': 8, 'Goldfish': 9, 'Gerbil': 10, 'Llama': 11, 'Hare': 12}
+
     # THE REST IS UP TO YOU
-    pass
+    pet_id = 1
+    for item in json_data: 
+        conn.execute('insert into Patients (pet_id, name, species_id, age, cuteness, aggresiveness) values(?,?,?,?,?,?)', (pet_id, item['name'], species_id_dict[item['species']], item['age'], item['cuteness'], item['aggressiveness']))
+        pet_id += 1
+        conn.commit()
+    
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute('select name from Patients where aggresiveness <= 10')
+    names = cur.fetchall()
+    conn.commit()
+    return names
 
 
 
